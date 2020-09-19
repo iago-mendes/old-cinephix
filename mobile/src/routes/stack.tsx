df9b304aa79f34eb2aacec2744b5793c344036d0
Iago <iagobrazmendes@gmail.com>
2020-09-19 11:59:30 -0300

    try to use animations [failed]

--- mobile/src/routes/stack.tsx
+++ mobile/src/routes/stack.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import {createStackNavigator} from '@react-navigation/stack'
+import { createStackNavigator } from '@react-navigation/stack'
+import { zoomIn, zoomOut } from 'react-navigation-transitions'
 
 interface StackProps
 {
@@ -13,9 +14,17 @@ const Stack: React.FC<StackProps> = ({screens}) =>
         <Navigator.Navigator
             headerMode='none'
             initialRouteName='Home'
+            screenOptions=
+            {{
+                // transitionSpec: {open: zoomIn(100), close: zoomOut(100)}
+            }}
         >
             {screens.map(screen => (
-                <Navigator.Screen key={screen.name} name={screen.name} component={screen.component} />
+                <Navigator.Screen
+                    key={screen.name}
+                    name={screen.name}
+                    component={screen.component}
+                />
             ))}
         </Navigator.Navigator>
     )
